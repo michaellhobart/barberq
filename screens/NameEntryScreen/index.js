c7efcea93e42fcfe6a5cac98e094a68f5ff65aba
Michael Hobart <michaelhobart@Michaels-iMac.local>
2017-12-18 14:47:38 -0500

    Fixed keyboard view and added a next button

--- screens/NameEntryScreen/index.js
+++ screens/NameEntryScreen/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import {View, Text, TextInput, Button, KeyboardAvoidingView, TouchableOpacity, TouchableWithoutFeedback, Keyboard} from 'react-native';
 
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+
+import { Icon } from 'react-native-elements';
 
 const NameEntryScreen = (props) => {
   if (props.currentScreen != "NameEntry") {
     return null
   }
   return (
-    <View style={{flex: 1, width: 1010}}>
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', marginBottom: 100}}>
-          {/* <Text style={{color: 'red', fontSize: 60, marginBottom: 35}}>{props.pendingCustomer}</Text> */}
+    <View style={{flex: 1, width: 1000}}>
+      <KeyboardAwareScrollView
+        style={{ backgroundColor: '#1b1b1b' }}
+        resetScrollToCoords={{ x: 0, y: 0 }}
+        contentContainerStyle={{marginTop: 185}}
+        scrollEnabled={false}
+      >
+        <View style={{alignItems: 'center', justifyContent: 'center'}}>
           <View style={{marginBottom: 75}}>
             <View style={{marginBottom: 10}}>
               <Text style={{fontSize: 60, fontWeight: '200', color: "#ecf0f1", textAlign: "center", letterSpacing: 2}}>
@@ -25,21 +32,25 @@ const NameEntryScreen = (props) => {
                 below.
             </Text>
           </View>
-            <KeyboardAvoidingView behavior="padding">
+
+            <View style={{flexDirection: 'row'}}>
               <TextInput
-                style={{height:110, width: 800, textAlign:'center', fontSize: 50, fontWeight: '700', color: "#ecf0f1", backgroundColor: '#1e1e1e', borderBottomWidth: 2, borderBottomColor: '#262626'}}
+                style={{marginLeft: 100, height:110, width: 700, textAlign:'center', fontSize: 50, fontWeight: '700', color: "#ecf0f1", backgroundColor: '#1e1e1e', borderBottomWidth: 2, borderBottomColor: '#262626'}}
                 autoCapitalize={'words'}
                 autoCorrect={false}
                 keyboardType={'default'}
-                returnKeyType={'next'}
+                returnKeyType={'done'}
                 keyboardAppearance={'dark'}
                 onChangeText={props.changeInputTextValue}
                 value={props.pendingCustomer}
-                // placeholder="TOUCH HERE"
-                // placeholderTextColor={"#ecf0f1"}
-                onEndEditing={props.navigateToBarbers}
+                onEndEditing={Keyboard.dismiss}
               />
-            </KeyboardAvoidingView>
+              <View style={{marginLeft: 30}}>
+                <Icon name='chevron-right' type='entypo' color='#ecf0f1' size={95} onPress={props.navigateToBarbers}/>
+              </View>
+            </View>
+
+            {/* </KeyboardAvoidingView> */}
             {/* <View style={{marginTop: 40}}>
               <Button onPress={props.navigateToBarbers} title="Please Select a Barber"/>
             </View> */}
@@ -49,8 +60,9 @@ const NameEntryScreen = (props) => {
             {/* <View style={{marginTop: 40}}>
               <Button onPress={props.logState} title="Console Log State"/>
             </View> */}
-          </View>
-      </TouchableWithoutFeedback>
+
+        </View>
+        </KeyboardAwareScrollView>
       <TouchableOpacity style={{width:125, height: 125, position: 'absolute', left:-10, top:650}} onPress={props.changeScreen}></TouchableOpacity>
     </View>
   )
